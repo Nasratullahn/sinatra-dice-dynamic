5460ac4588be6df7487604f4b20b519c711d2e1f
Nasratullah Nasrat <60459439+Nasratullahn@users.noreply.github.com>
2024-03-17 22:33:26 +0000

    num sides

--- app.rb
+++ app.rb
@@ -10,13 +10,13 @@ get("/dice/:rolls/:sides") do
   @num_dice = params.fetch("rolls").to_i
   @num_sides = params.fetch("sides").to_i 
   
-  @rolls = []
+  @roll = []
   
   @num_dice.times do
     
     die = rand(1..@num_sides)
 
-    @rolls.push(die)
+    @roll.push(die)
   end
 
   erb(:flexible)
